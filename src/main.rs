5d9729a3dbef1d70922f42123b1d8179ef733bf8
Alexandre Figueiredo <17xande@gmail.com>
2023-01-13 08:26:44 +0200

    move wallpaper code to own fn

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ use windows::{
     Win32::{System::Com::*, UI::Shell::*},
 };
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("starting");
+
+    change_wallpaper("C:\\Users\\17xan\\dev\\unsplash\\wallpaper.jpg");
+
+    Ok(())
+}
+
+fn change_wallpaper(path: &str) {
     unsafe {
         CoInitializeEx(None, COINIT_APARTMENTTHREADED).unwrap();
 
@@ -13,7 +21,7 @@ fn main() {
         let mdp = dw.GetMonitorDevicePathAt(0).unwrap();
         println!("monitor device path: {}", mdp.to_string().unwrap());
         let monitor_id = PCWSTR(mdp.as_ptr());
-        let img = &HSTRING::from("C:\\Users\\17xan\\dev\\unsplash\\wallpaper.jpg");
+        let img = &HSTRING::from(path);
         println!("desktop wallpaper path: {}", img.to_string());
         dw.SetWallpaper(monitor_id, img).unwrap();
 
